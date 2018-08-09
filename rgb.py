986dc3c09731e180f713e5b35f916e46f44da06f
bas <basilieus@gmail.com>
2018-08-08 21:59:45 -0700

    Training code done for RGB NN

--- rgb.py
+++ rgb.py
@@ -1,15 +1,19 @@
+# this 3 layer NN takes an input of R,G,B values
+# and tells you if the text should be black or white
+# input = [r,g,b]
+# output = [0|1]
+
 import sys
 import numpy as np
+
+np.random.seed(2)
+learning_rate = 0.5
 
 # activation function
 def sigmoid(x, d=False):
     if (d):
         return (x*(1-x))
     return 1/(1+np.exp(-x))
-
-# used to train!
-def sqrErr(t, o):
-    return .5 * ((t-o) ** 2)
 
 def train():
     # features = x = inputs
@@ -19,24 +23,66 @@ def train():
     labels = []
 
     # weights / synapses / connections
-    # syn0 is the
+    # syn0 is the weights for features -> hidden layers
+    # syn1 is the weights for hidden layers -> outputs
+    # 3,4 is the 3 inputs to 4 hidden nodes
+    # 4,1 is the 4 hidden nodes into the 1 output node
     syn0 = 2 * np.random.random((3,4)) - 1
     syn1 = 2 * np.random.random((4,1)) - 1
-    print syn0
 
-np.random.seed(2)
-learning_rate = 0.5
+    # loop 60000 iterations so the weights have enough
+    # times to adjust to lowering the error rate towards zero
+    for j in xrange(60000):
+        # our first layer are the inputs r, g, b
+        l0 = features
 
+        # below we "feed forward" through the network
+
+        # our second layer are the hidden layers
+        # we take the dot product of inputs and synapses
+        # and run them through the activation function
+        l1 = sigmoid(np.dot(l0, syn0))
+
+        # we take the hidden nodes and generate our third layer
+        # the output layer, and run them through the activation
+        # function to normalize probabilities
+        l2 = sigmoid(np.dot(l1, syn1))
+
+        # backward propagation where we update our weights
+
+        # this is the total error of our expected output (targets)
+        # and the current output
+        l2_error = labels - l2
+
+        # we need to take the mean squared error to find the actuall 
+        # error rate
+        if (j % 5000):
+            print 'Error Rate: ' + str(np.mean(np.abs(l2_error)))
+
+        # here we find the delta between our output layers
+        # and the deriviate of the weights
+        l2_delta = l2_error * sigmoid(l2, d=True)
+
+        # we find the hidden layer deltas by taking the dot product of
+        # the output deltas and output synapse weights
+        l1_error = l2_delta.dot(syn1.T)
+
+        # now we go back even further and use the hidden layer
+        # erorr rates and find the hidden layer deltas
+        l1_delta = l1_error * sigmoid(l1, d=true)
+
+        # now that we have the delates between the expected and output
+        # we can update our synapses/weights
+        syn1 += l1.T.dot(l2_delta)
+        syn0 += l0.T.dot(l1_delta)
+
+'''
 # inputs are R, G, B
 r,g,b = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
 inputs = [r,g,b]
 
-train()
 
 
-
-
-'''
 
 inputs r,g,b
 
